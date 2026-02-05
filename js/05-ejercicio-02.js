8c08bf17550ff408c2e510881f674bfdaf92a96e
Williams Hernández <hernandw@gmail.com>
2026-02-04 22:07:49 -0300

    :sparkles: ejercicio 3

--- js/05-ejercicio-02.js
+++ js/05-ejercicio-02.js
@@ -23,3 +23,31 @@ depositar(monto)
 retirar(monto)
 
 mostrarSaldo() */
+
+class CuentaBancaria {
+  constructor(titular, saldoInicial = 0) {
+    this.titular = titular;
+    this.saldo = saldoInicial;
+  }
+
+  depositar(monto) {
+    if (monto <= 0) {
+      return "El monto a depositar debe ser mayor a 0";
+    }
+    this.saldo += monto;
+    return `Deposito exitoso. Nuevo Saldo: ${this.saldo}`
+  }
+
+  mostrarSaldo(){
+    return `
+    Saldo actual de ${this.titular} es: ${this.saldo}
+    `
+  }
+}
+
+
+const cuenta1 = new CuentaBancaria("Walter", 1000)
+
+console.log(cuenta1.depositar(500))
+console.log(cuenta1.depositar(500))
+console.log(cuenta1.depositar(3000))
